03ffbd7da0c3fbb7fd8d164178ada575b89ce9f8
dits <mayocuak@gmail.com>
2026-08-08 14:56:47 +0700

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import DashboardShell from '@/components/DashboardShell';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <DashboardShell>{children}</DashboardShell>;
 }
