580c271b46a1896b9a8ad3c942160c67c76a0dfe
dits <mayocuak@gmail.com>
2026-08-13 00:20:39 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#D4537E',
+  themeColor: '#0b0e14',
   width: 'device-width',
   initialScale: 1,
   viewportFit: 'cover',
